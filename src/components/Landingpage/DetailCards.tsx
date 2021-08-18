9d05117a7b57a9a8d502d1bc63b3e80b1f5681ad
Francisco <fd07ass@gmail.com>
2021-08-18 16:31:29 -0300

    landingpage/cards

---
+++ src/components/Landingpage/DetailCards.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import '../../styles/components/Landingpage/DetailCards.scss'
+
+type Card = {
+    title: string,
+    paragraph: string,
+}
+
+export function DetailCards({title, paragraph}: Card) {
+    return (
+        <div id='card'>
+            <h3>{title}</h3>
+            <p>{paragraph}</p>
+        </div>
+    )
+}
--- src/pages/Landingpage/index.tsx
+++ src/pages/Landingpage/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Home } from '../../components/Landingpage/Home';
-import {DiagonalBox} from '../../components/Landingpage/DiagonalBox';
+import { DiagonalBox } from '../../components/Landingpage/DiagonalBox';
+import { DetailCards } from '../../components/Landingpage/DetailCards';
 
-export function Landingpage(){
+export function Landingpage() {
     return (
         <>
-            <Home/>
-            <DiagonalBox/>
+            <Home />
+            <DiagonalBox />
+            <DetailCards
+                title="Videoaulas"
+                paragraph="
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua. 
+                    Neque gravida in fermentum et. 
+                    Lobortis mattis aliquam faucibus purus in. 
+                    Aliquam nulla facilisi cras fermentum. 
+                    Mauris augue neque gravida in. "
+            />
         </>
     )
 }
